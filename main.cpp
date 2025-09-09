0b86e4c14835f75f5a55bc26b34125c67f47f6df
Valentín Tuesta <valentin.tuesta@utec.edu.pe>
2025-09-09 11:32:31 -0500

    clear and operator[]

--- main.cpp
+++ main.cpp
@@ -19,6 +19,41 @@ public:
         head->next = head;
         head->prev = head;
     }
+
+    void clear() {
+        auto pt1 = head->next;
+        while(pt1->next != head) {
+            auto temp = pt1;
+            pt1 = pt1->next;
+            delete temp;
+        }
+        head->next = head;
+        head->prev = head;
+    }
+
+    int size() {
+        return 0;
+    }
+
+    bool empty() {
+        return 0;
+    }
+
+    T operator[](int pos) {
+        if(pos<0 or pos>=this->size()) {
+            throw std::out_of_range("Index out of range");
+        }
+        if(this->empty()) {
+            throw std::out_of_range("List is empty");
+        }
+        Node<T>* temp = head->next;
+        int i = 0;
+        while (i <= pos) {
+            temp = temp->next;
+            i++;
+        }
+        return temp->data;
+    }
 };
 
 int main(){
